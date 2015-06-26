9de2388c235b6f654c38a7d8ae4fd6005a9de881
Toshinori Sato <overlasting@gmail.com>
2015-06-26 12:19:29 +0900

    Fix prefix strings of lines which executes echo

--- libexec/install-mecab-and-mecab-ipadic.sh
+++ libexec/install-mecab-and-mecab-ipadic.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 BASEDIR=$(cd $(dirname $0);pwd)
-SCRIPT_NAME="[install-mecab-and-mecab-ipadic] :"
+ECHO_PREFIX="[install-mecab-and-mecab-ipadic] :"
 
-echo "$SCRIPT_NAME Get sudo password"
+echo "${ECHO_PREFIX} Get sudo password"
 sudo pwd
 
 sudo rpm -ivh http://packages.groonga.org/centos/groonga-release-1.1.0-1.noarch.rpm
 sudo yum install -y mecab mecab-devel mecab-ipadic
 
-echo "$SCRIPT_NAME Finish.."
+echo "${ECHO_PREFIX} Finish.."
