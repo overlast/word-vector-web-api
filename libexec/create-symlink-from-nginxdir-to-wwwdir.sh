da1db92070573fbac811abcba4346b4297701768
Toshinori Sato <overlasting@gmail.com>
2015-06-26 10:41:24 +0900

    Add process to check existance of www directory

---
+++ libexec/create-symlink-from-nginxdir-to-wwwdir.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BASEDIR=`cd $(dirname $0); pwd`
+ECHO_PREFIX="[link_from_nginx_html_dir_to_www_dir]"
+WWW_DIR=$BASEDIR/../www/
+
+NGINX_DIR=/usr/local/nginx-with-msgpack-rpc-module/
+
+echo "${ECHO_PREFIX} Create symlink to www dir"
+
+if [ -d ${NGINX_DIR} ] ;then
+    if [ -d ${WWW_DIR} ]; then
+        sudo ln -s ${WWW_DIR} ${NGINX_DIR}
+    else
+        echo
+        echo "${ECHO_PREFIX} ${WWW_DIR} isn't exist.."
+        echo
+    fi
+else
+    echo
+    echo "${ECHO_PREFIX} ${NGINX_DIR} isn't exist.."
+    echo
+fi
+
+echo "${ECHO_PREFIX} Finish !!"
