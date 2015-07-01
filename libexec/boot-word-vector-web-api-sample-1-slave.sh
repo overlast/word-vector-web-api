da14f67667e818b1d55e2bd160e1b6c23d840439
Toshinori Sato <overlasting@gmail.com>
2015-07-02 04:40:30 +0900

    Add process to keep processes for demo

--- libexec/boot-word-vector-web-api-sample-1-slave.sh
+++ libexec/boot-word-vector-web-api-sample-1-slave.sh
@@ -3,3 +3,8 @@
 /usr/local/bin/word2vec-msgpack-rpc-server -m /var/tmp/sample-word-vector-web-api/jawiki.20150602.neologd.bin -p 22676 > /dev/null 2>&1 &
 /usr/local/nginx-with-msgpack-rpc-module/sbin/nginx -c /root/git/word-vector-web-api/conf/sample-word-vector-web-api-slave1.conf
 /usr/local/nginx-with-msgpack-rpc-module/sbin/nginx -c /root/git/word-vector-web-api/conf/sample-word-vector-web-api-master-solo.conf
+
+while :
+do
+    sleep 1
+done
